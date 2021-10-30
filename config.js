a701125d9b40c438cdade841e5655ed6fc608821
quocbao1097 <20521097@gm.uit.edu.vn>
2021-10-30 10:23:29 +0700

    thiep mung

---
+++ config.js
@@ -0,0 +1,30 @@
+const nameGirl = 'Thảo Hiền';
+const giftUrl = 'https://www.facebook.com/herlocksholmes1854';
+const eventName = 'Happy Birtday ';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc em sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = './assets/Album-PhotoShop.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+
+
+
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
