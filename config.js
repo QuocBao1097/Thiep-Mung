ae0b8f366cfef51a0d6ff52e90695757b5441b53
quocbao1097 <20521097@gm.uit.edu.vn>
2021-11-13 18:47:23 +0700

    hi

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Thảo Hiền';
 const giftUrl = 'https://www.facebook.com/herlocksholmes1854';
 const eventName = 'Happy Birtday ';
-const titleCard = 'Tặng người ấy';
+const titleCard = 'Tặng Em Yêu';
 const contentCard = 'Chúc em sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 const giftImage = './assets/Album-PhotoShop.jpg';
 const base64 = '';
